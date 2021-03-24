51f1962d6e2d433f533e85f3e55715d4870f9c41
Vitaliya_keks <73116690+VitaliiPysyniuk@users.noreply.github.com>
2021-03-24 23:46:01 +0200

    fixed Executor

--- src/Executor.java
+++ src/Executor.java
@@ -4,5 +4,5 @@ import java.rmi.RemoteException;
 public interface Executor extends Remote {
     String pingExecutor(long time) throws RemoteException;
     int add(int a, int b) throws  RemoteException;
-    double[][] createMatrix(int n, double max, double min);
+    double[][] createMatrix(int n, double max, double min) throws RemoteException;
 }
