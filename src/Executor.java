5ebd44ca289b4feaa2291cc66404907a95f7b14a
Vitaliya_keks <73116690+VitaliiPysyniuk@users.noreply.github.com>
2021-03-24 23:57:31 +0200

    changed Server

--- src/Executor.java
+++ src/Executor.java
@@ -2,7 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Executor extends Remote {
-    String pingExecutor(long time) throws RemoteException;
+    String ping(long time) throws RemoteException;
     int add(int a, int b) throws  RemoteException;
     double[][] createMatrix(int n, double max, double min) throws RemoteException;
 }
--- src/Server.java
+++ src/Server.java
@@ -8,7 +8,7 @@ public class Server implements Executor {
     private static Server serviceImpl = null;
     public Server() { }
 
-    public String pingExecutor(long time) {
+    public String ping(long time) {
         System.out.println("Server is active");
 //        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
 //        Date date = new Date(time);
@@ -36,15 +36,19 @@ public class Server implements Executor {
 
     public static void main(String[] args) {
         try {
+            System.out.println("Server started...");
             System.setProperty("java.rmi.server.hostname","ec2-34-194-59-42.compute-1.amazonaws.com");
-            Server obj = new Server();
-            serviceImpl = obj;
+//            Server obj = new Server();
+//            serviceImpl = obj;
             Registry registry = LocateRegistry.createRegistry(8080);
-            Executor stub = (Executor) UnicastRemoteObject.exportObject(serviceImpl, 8080);
+//            Executor stub = (Executor) UnicastRemoteObject.exportObject(serviceImpl, 8080);
+            Executor stub = (Executor) UnicastRemoteObject.exportObject(new Server(), 8080);
             registry.rebind("Executor", stub);
-            System.err.println("Server ready");
-            System.out.println("Registry list:");
-            System.out.println(registry.list()[0]);
+            System.out.println("Server ready...");
+            System.out.println("Registry list: " + registry.list()[0] + "\n");
+            new Scanner(System.in).nextLine();
+            System.out.println("Server stopped...");
+            System.exit(0);
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
