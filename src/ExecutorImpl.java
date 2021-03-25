95b2aaaeffe34eaa80a8c8fbbbc94fb92047ce2e
Vitaliya_keks <73116690+VitaliiPysyniuk@users.noreply.github.com>
2021-03-25 17:18:00 +0200

    add calcR51 and calcR61

--- src/ExecutorImpl.java
+++ src/ExecutorImpl.java
@@ -7,7 +7,7 @@ public class ExecutorImpl implements Executor {
     }
 
     public double[][] calculateR10(int n) {
-        System.out.println("calculated C2");
+        System.out.println("calculated R10");
         R10 = new double[n][n];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
@@ -16,7 +16,7 @@ public class ExecutorImpl implements Executor {
     }
 
     public double[][] calculateR16(int n) {
-        System.out.println("Calculated B");
+        System.out.println("Calculated R16");
         R16 = new double[n][1];
         for (int i = 0; i < n; i++)
             R16[i][0] = 20 * (Math.pow(i, 3) + 20);
@@ -24,26 +24,31 @@ public class ExecutorImpl implements Executor {
     }
 
     public double[][] calculateR20(double[][] a, double[][] b) {
+        System.out.println("Calculated R20");
         R20 =  Matrix.mult(a, b);
         return R20;
     }
 
     public double[][] calculateR32(double[][] a) {
+        System.out.println("Calculated R32");
         R32 =  Matrix.mult(a, R16);
         return R32;
     }
 
     public double[][] calculateR42(double[][] a) {
+        System.out.println("Calculated R42");
         R42 =  Matrix.mult(a, R32);
         return R42;
     }
 
     public double[][] calculateR51(double[][] a, double[][] b) {
+        System.out.println("Calculated R51");
         R51 =  Matrix.mult(a, b);
         return R51;
     }
 
     public double[][] calculateR61(double[][] a) {
+        System.out.println("Calculated R61");
         R61 =  Matrix.mult(a, R51);
         return R61;
     }
