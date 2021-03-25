8f02cafb7678a7090f47ace8d012f54d95617969
Vitaliya_keks <73116690+VitaliiPysyniuk@users.noreply.github.com>
2021-03-25 02:35:54 +0200

    changes v7

--- src/ExecutorImpl.java
+++ src/ExecutorImpl.java
@@ -1,6 +1,6 @@
 public class ExecutorImpl implements Executor {
-//    ICalculation calc, multiplication, multiplicationT, subtraction, addition;
-//    LambdaExecutor lambdaExecutor = new LambdaExecutor();
+    ICalculation calc, multiplication, multiplicationT, subtraction, addition;
+    LambdaExecutor lambdaExecutor = new LambdaExecutor();
     static double[][] C2;
 
     public String ping() {
@@ -19,72 +19,71 @@ public class ExecutorImpl implements Executor {
 
     public double[][] calculateC2(int n) {
         System.out.println("calculateC2");
-//        calc = (a, b) -> {
-//            double[][] ab = new double[n][n];
-//            for (int i = 0; i < n; i++)
-//                for (int j = 0; j < n; j++)
-//                    ab[j][i] = 20 * (Math.pow(i, 3) - Math.pow(j, 3) + 2);
-//            return ab;
-//        };
-//        new CalculationProcess(calc, lambdaExecutor, ExecutorImpl.C2);
-//        return ExecutorImpl.C2;
-        double[][] ab = new double[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                ab[j][i] = 20 * (Math.pow(i, 3) - Math.pow(j, 3) + 2);
-        return ab;
+        calc = (a, b) -> {
+            double[][] ab = new double[n][n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    ab[j][i] = 20 * (Math.pow(i, 3) - Math.pow(j, 3) + 2);
+            return ab;
+        };
+        CalculationProcess calculationProcess = new CalculationProcess(calc, lambdaExecutor, ExecutorImpl.C2);
+        try {
+            calculationProcess.process.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return calculationProcess.result;
     }
 }
 
-//interface ICalculation {
-//    double[][] doCalculation(double[][] a, double[][] b);
-//}
-//
-//
-//class LambdaExecutor {
-//    public void doCalculation(ICalculation ref, double[][] a, double[][] b, double[][] var) {
-//        var = ref.doCalculation(a, b);
-////        if (Decomposition.showІntermResults) {
-//        if (false) {
-//            System.out.println("\t\tCalculation result: ");
-//            Matrix.show(var);
-//        }
-//    }
-//}
-//
-////потік виконання обчислень
-//class CalculationProcess implements Runnable {
-//    LambdaExecutor executor;
-//    ICalculation calc;
-//    double[][] a = {{}};
-//    double[][] b = {{}};
-//    double[][] var;
-//    Thread process;
-//
-//    public CalculationProcess(ICalculation _calc, LambdaExecutor _executor, double[][] _a, double[][] _b, double[][] _var) {
-//        this.calc = _calc;
-//        this.executor = _executor;
-//        this.a = _a;
-//        this.b = _b;
-//        this.var = _var;
-//        this.process = new Thread(this);
-//        this.process.start();
-//    }
-//
-//    public CalculationProcess(ICalculation _calc, LambdaExecutor _executor, double[][] _var) {
-//        this.calc = _calc;
-//        this.executor = _executor;
-//        this.var = _var;
-//        this.process = new Thread(this);
-//        this.process.start();
-//    }
-//
-//    public void run() {
-//        synchronized (executor) {
-////            if (Decomposition.showІntermResults)
-//            if (false)
-//                System.out.println("\n" + this.process.getName() + " execute calculation: ");
-//            executor.doCalculation(calc, a, b, var);
-//        }
-//    }
-//}
+interface ICalculation {
+    double[][] doCalculation(double[][] a, double[][] b);
+}
+
+
+class LambdaExecutor {
+    public double[][] doCalculation(ICalculation ref, double[][] a, double[][] b) {
+        double[][] result = ref.doCalculation(a, b);
+//        if (Decomposition.showІntermResults) {
+        if (false) {
+            System.out.println("\t\tCalculation result: ");
+            Matrix.show(result);
+        }
+        return result;
+    }
+}
+
+//потік виконання обчислень
+class CalculationProcess implements Runnable {
+    public Thread process;
+    public double[][] result;
+    LambdaExecutor executor;
+    ICalculation calc;
+    double[][] a;
+    double[][] b;
+
+    public CalculationProcess(ICalculation _calc, LambdaExecutor _executor, double[][] _a, double[][] _b) {
+        this.calc = _calc;
+        this.executor = _executor;
+        this.a = _a;
+        this.b = _b;
+        this.process = new Thread(this);
+        this.process.start();
+    }
+
+    public CalculationProcess(ICalculation _calc, LambdaExecutor _executor, double[][] _var) {
+        this.calc = _calc;
+        this.executor = _executor;
+        this.process = new Thread(this);
+        this.process.start();
+    }
+
+    public void run() {
+        synchronized (executor) {
+//            if (Decomposition.showІntermResults)
+            if (false)
+                System.out.println("\n" + this.process.getName() + " execute calculation: ");
+            result = executor.doCalculation(calc, a, b);
+        }
+    }
+}
